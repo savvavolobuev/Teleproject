bb6015de3ccb07444caa1394e96354ff7a084872
Alex K <Rendal88@gmail.com>
2014-10-30 01:08:20 +0400

    login activity skeleton

---
+++ app/src/main/java/fl/developer/teleproject/LoginActivity.java
@@ -0,0 +1,52 @@
+package fl.developer.teleproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+/**
+ * Created by alexk on 29.10.2014.
+ */
+public class LoginActivity extends Activity {
+
+    ImageButton closeButton;
+    EditText passwordEdit;
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        closeButton = (ImageButton) findViewById(R.id.closeButton);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginActivity.this.finish();
+            }
+        });
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setEnabled(false);
+        passwordEdit = (EditText) findViewById(R.id.password);
+        passwordEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                 loginButton.setEnabled(editable.length() > 0);
+            }
+        });
+    }
+}
