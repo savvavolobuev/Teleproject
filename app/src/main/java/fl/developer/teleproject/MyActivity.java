aef82d259ef96feb76827d8f9401ba1784decbf0
Alex K <Rendal88@gmail.com>
2014-10-30 00:56:26 +0400

    login activity skeleton

--- app/src/main/java/fl/developer/teleproject/MyActivity.java
+++ app/src/main/java/fl/developer/teleproject/MyActivity.java
@@ -1,17 +1,30 @@
 package fl.developer.teleproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MyActivity extends Activity {
+
+    private Button testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        testButton = (Button) findViewById(R.id.loginTestButton);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MyActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
